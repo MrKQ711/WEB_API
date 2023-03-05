809a8f1a16a49f5a336532f0c3e0704031acf4c3
Thái <92800620+MrKQ711@users.noreply.github.com>
2023-03-05 11:48:08 +0700

    Update update.php

--- rest_api_php/webapp/v1/update.php
+++ rest_api_php/webapp/v1/update.php
@@ -1,5 +1,11 @@
 <?php
 
+use Firebase\JWT\Key;
+
+    // include vendor
+    require_once ("../vendor/autoload.php");
+    use \Firebase\JWT\JWT;
+
     // include headers
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
@@ -9,7 +15,7 @@
     // include database.php
     include_once ("../config/database.php");
     // include student.php
-    include_once ("../classes/student.php");
+    include_once ("../classes/Users.php");
 
     // create object of Database class
     $db = new Database();
@@ -17,38 +23,69 @@
     $connection = $db->connect();
 
     // create object of Student class
-    $student = new Student($connection);
+    $user_obj = new Users($connection);
 
     if($_SERVER['REQUEST_METHOD'] === "POST"){
-        // get the data from the form
-        $data = json_decode(file_get_contents("php://input"));
 
-        if(!empty($data->id) && !empty($data->name) && !empty($data->email) && !empty($data->mobile)){
+        $header = getallheaders();
 
-            // set the data to the variables
-            $student->id = $data->id;
-            $student->name = $data->name;
-            $student->email = $data->email;
-            $student->mobile = $data->mobile;
+        if(empty($header['Authorization'])){
+            http_response_code(500);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Access denied."
+            ));
+            return;
+        } else {
+            $jwt = $header['Authorization'];
+        }
 
-            if($student->update_student()){
-                // set response code
-                http_response_code(200); // 200 means ok
-                // display message
-                echo json_encode(array("status" => 1, "message" => "Data updated successfully"));
+        try {
+            $secret_key = "owt125";
+
+            $decode_data = JWT::decode($jwt, new Key($secret_key, 'HS512'));
+
+            $user_obj->id = $decode_data->data->id;
+
+            $data = json_decode(file_get_contents("php://input"));
+
+            if(!empty($data->name) && !empty($data->email) && !empty($data->password)){
+                
+                
+                // set the data to the variables
+                //$user_obj->id = $data->id;
+                $user_obj->name = $data->name;
+                $user_obj->email = $data->email;
+                $user_obj->password = password_hash($data->password, PASSWORD_DEFAULT);
+    
+                if($user_obj->update_user()){
+                    // set response code
+                    http_response_code(200); // 200 means ok
+                    // display message
+                    echo json_encode(array("status" => 1, "message" => "Data updated successfully"));
+                } else {
+                    // set response code
+                    http_response_code(500); // 500 means internal server error
+                    // display message
+                    echo json_encode(array("status" => 0, "message" => "Failed to update data"));
+                }
+    
             } else {
                 // set response code
-                http_response_code(500); // 500 means internal server error
+                http_response_code(404); // 404 means not found
                 // display message
-                echo json_encode(array("status" => 0, "message" => "Failed to update data"));
+                echo json_encode(array("status" => 0, "message" => "All fields are required"));
             }
 
-        } else {
-            // set response code
-            http_response_code(404); // 404 means not found
-            // display message
-            echo json_encode(array("status" => 0, "message" => "All fields are required"));
-        }
+        } catch (Exception $ex) {
+            http_response_code(500);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Access denied.",
+                "error" => $ex->getMessage()
+            ));
+            return;
+        }    
 
     } else {
         // set response code
