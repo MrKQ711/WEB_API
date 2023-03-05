8ab777a35acc3948f5d42709ccf7048fd17e3586
Thái <92800620+MrKQ711@users.noreply.github.com>
2023-03-05 11:46:42 +0700

    Update list-all-projects-api.php

--- rest_api_php/webapp/v1/list-all-projects-api.php
+++ rest_api_php/webapp/v1/list-all-projects-api.php
@@ -1,5 +1,11 @@
 <?php
 
+use Firebase\JWT\Key;
+
+    // include vendor
+    require_once ("../vendor/autoload.php");
+    use \Firebase\JWT\JWT;
+
     ini_set('display_errors', 1); 
     // include headers
     header("Access-Control-Allow-Origin: *");
@@ -16,40 +22,88 @@
     $user_obj = new Users($connection);
 
     if($_SERVER['REQUEST_METHOD'] === "GET"){
-        
-        $projects = $user_obj->get_all_projects();
 
-        if($projects->num_rows > 0){
-            $projects_arr = array();
-            $projects_arr['data'] = array();
 
-            while($row = $projects->fetch_assoc()){
-                extract($row);
+        $header = getallheaders();
 
-                $project_item = array(
-                    "id" => $id,
-                    "name" => $name,
-                    "description" => $description,
-                    "user_id" => $user_id,
-                    "status" => $status,
-                    "created_at" => $created_at
-                );
-
-                array_push($projects_arr['data'], $project_item);
-            }
-
-            http_response_code(200);
-            echo json_encode(array(
-                "status" => 1,
-                "message" => "Projects found.",
-                "data" => $projects_arr
-            ));
-        } else {
-            http_response_code(404);
+        if (empty($header['Authorization'])) {
+            http_response_code(500);
             echo json_encode(array(
                 "status" => 0,
-                "message" => "No projects found."
+                "message" => "Access denied."
             ));
+            return;
+        } else {
+            $jwt = $header['Authorization'];
         }
+
+        //$jwt = $header['Authorization'];
+        
+
+        try{
+
+            $secret_key = "owt125";
+
+            $decode_data = JWT::decode($jwt, new Key($secret_key, 'HS512'));
+
+            // how to get username from $decode_data
+            $user_obj->name = $decode_data->data->name;
+            $user_obj->email = $decode_data->data->email;
+
+            if($user_obj->name == "Thai_V5" && $user_obj->email == "thai@gmail.com"){
+
+                $user_obj->user_id = $decode_data->data->id;
+
+                $projects = $user_obj->get_all_projects();
+
+                if($projects->num_rows > 0){
+                    $projects_arr = array();
+                    $projects_arr['data'] = array();
+    
+                    while($row = $projects->fetch_assoc()){
+                        extract($row);
+    
+                        $project_item = array(
+                            "id" => $id,
+                            "name" => $name,
+                            "description" => $description,
+                            "user_id" => $user_id,
+                            "status" => $status,
+                            "created_at" => $created_at
+                        );
+    
+                        array_push($projects_arr['data'], $project_item);
+                    }
+    
+                    http_response_code(200);
+                    echo json_encode(array(
+                        "status" => 1,
+                        "message" => "Projects found.",
+                        "data" => $projects_arr
+                    ));
+                } else {
+                    http_response_code(404);
+                    echo json_encode(array(
+                        "status" => 0,
+                        "message" => "No projects found."
+                    ));
+                }
+            } else {
+                http_response_code(500);
+                echo json_encode(array(
+                    "status" => 0,
+                    "message" => "You don't have permission to access."
+                ));
+            }
+       
+        }catch(Exception $e){
+            http_response_code(500);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Access denied.",
+                "error" => $e->getMessage()
+            ));
+        }     
     }
+
 ?>
