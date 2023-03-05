82862cee7bf2149d287d7f6246670e251b515ac7
Thái <92800620+MrKQ711@users.noreply.github.com>
2023-03-05 11:46:17 +0700

    Update delete.php

--- rest_api_php/webapp/v1/delete.php
+++ rest_api_php/webapp/v1/delete.php
@@ -1,5 +1,12 @@
 <?php
 
+
+use Firebase\JWT\Key;
+
+    // include vendor
+    require_once ("../vendor/autoload.php");
+    use \Firebase\JWT\JWT;
+
     ini_set('display_errors', 1);
     // include headers
     header("Access-Control-Allow-Origin: *");
@@ -9,7 +16,7 @@
     // include database.php
     include_once ("../config/database.php");
     // include student.php
-    include_once ("../classes/student.php");
+    include_once ("../classes/Users.php");
 
     // create object of Database class
     $db = new Database();
@@ -17,15 +24,36 @@
     $connection = $db->connect();
 
     // create object of Student class
-    $student = new Student($connection);
+    $user_obj = new users($connection);
 
     if($_SERVER['REQUEST_METHOD'] === "GET") {
+
+        $header = getallheaders();
+
+        if(empty($header['Authorization'])){
+            http_response_code(500);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Access denied."
+            ));
+            return;
+        } else {
+            $jwt = $header['Authorization'];
+        }
+
+        try {
+            $secret_key = "owt125";
+
+            $decode_data = JWT::decode($jwt, new Key($secret_key, 'HS512'));
+
+            $user_obj->id = $decode_data->data->id;
+
             
-            $student_id = isset($_GET['id']) ? intval($_GET['id']) : "";
-    
-            if(!empty($student_id)){
-                $student->id = $student_id;
-                if($student->delete_student()){
+
+            // if(!empty($data->id)){
+                //$student->id = $student_id;
+                $data = $user_obj->delete_user();
+                if(!empty($data)){
                     // set response code
                     http_response_code(200); // 200 means ok
                     // display message
@@ -36,12 +64,24 @@
                     // display message
                     echo json_encode(array("status" => 0, "message" => "Failed to delete data"));
                 }
-            } else {
-                // set response code
-                http_response_code(404); // 404 means not found
-                // display message
-                echo json_encode(array("status" => 0, "message" => "No data found"));
-            }
+            // } else {
+            //     // set response code
+            //     http_response_code(404); // 404 means not found
+            //     // display message
+            //     echo json_encode(array("status" => 0, "message" => "No data found"));
+            // }
+
+        } catch (Exception $ex) {
+            http_response_code(500);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Access denied."
+            ));
+            return;
+        }  
+            $student_id = isset($_GET['id']) ? intval($_GET['id']) : "";
+    
+            
     
         } else {
             // set response code
